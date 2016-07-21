b892ed442c40cddd2bf4053fb49656a82636eb62
qds-hoi <hoi.dang@quodisys.com>
2016-07-21 15:10:39 +0700

    init project

---
+++ suracare/suracare/App/Helpers/rSConstant.swift
@@ -0,0 +1,13 @@
+//
+//  rSConstant.swift
+//  suracare
+//
+//  Created by hoi on 7/21/16.
+//  Copyright © 2016 Sugar Rain. All rights reserved.
+//
+
+import Foundation
+
+struct rSConstant {
+    let baseReference: String = "https://sugarrain.firebaseio.com"
+}
---
+++ suracare/suracare/App/Model/rSAppModel.swift
@@ -0,0 +1,16 @@
+//
+//  rSAppModel.swift
+//  suracare
+//
+//  Created by hoi on 7/21/16.
+//  Copyright © 2016 Sugar Rain. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class rSAppModel {
+    
+    
+    
+}
