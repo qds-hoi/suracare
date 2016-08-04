ad2ab391c7b4a77880f3e5bf5cb74ba57d968567
qds-hoi <hoi.dang@quodisys.com>
2016-08-04 15:02:47 +0700

    add on board view controller

---
+++ suracare/suracare/App/ViewControllers/OnboardViewController/OnboardViewController.swift
@@ -0,0 +1,35 @@
+//
+//  OnboardViewController.swift
+//  suracare
+//
+//  Created by hoi on 8/4/16.
+//  Copyright © 2016 Sugar Rain. All rights reserved.
+//
+
+import UIKit
+
+class OnboardViewController: rSBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
